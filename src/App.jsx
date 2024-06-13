6dbeff87fb8e85ac2dc62312c575ce70cb021b22
alefew <alefewyimer4@gmail.com>
2024-06-13 16:56:16 +0300

    reusable components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import reactImg from './assets/react-core-concepts.png';
+import reactImg from "./assets/react-core-concepts.png";
+import componentImg from "./assets/components.png";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -20,12 +21,31 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core concepts</h2>
+          <ul>
+            <CoreConcept
+              title="components"
+              description="The core UI building block"
+              image={componentImg}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
