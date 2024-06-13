b358276b14803488c09e056740db69691295c844
alefew <alefewyimer4@gmail.com>
2024-06-13 19:10:17 +0300

    special child props with examles section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButtons.jsx";
 
 function App() {
   return (
@@ -19,6 +20,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
           </ul>
+        </section>
+        <section id="examples">
+            <h2>Examples</h2>
+            <menu>
+                <TabButton>Components</TabButton>
+                <TabButton>JSX</TabButton>
+                <TabButton>Props</TabButton>
+                <TabButton>State</TabButton>
+            </menu>
         </section>
       </main>
     </div>
